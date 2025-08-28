47aecc2ac17fca35847b7a248d1309225c595a10
DeveloperWK <wasifulkabir2023@gmail.com>
2025-08-28 10:10:35 +0600

    Add Numeric Operations project with initial implementation and configuration files

---
+++ Numeric_Operations/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let sum = 5+20;
+    let difference = 100-50;
+    let multiply = 50*90;
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3;
+    let remainder = 5 % 3;
+    println!("{}",sum);
+    println!("{}",difference);
+    println!("{}",multiply);
+    println!("{}",quotient);
+    println!("{}",truncated);
+    println!("{}",remainder);
+}
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,19 @@
+use std::any::type_name;
+
+
+fn type_of<T>(_:T)->&'static str {
+    type_name::<T>()
+}
+
 fn main() {
     // Mut Stand for Mutable
-    let mut x = 12;
-    x =23;
-    println!("this is number {x}");
+    // let mut x = 12;
+    // x =23;
+    let y:i8 = 127;
+    println!("{}",y);
+    let d = 255;
+    let c = y.wrapping_add(10);
+    println!("c:{}",c);
+    println!("{}",type_of(d));
+
 }
